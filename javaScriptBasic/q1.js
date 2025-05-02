3a7d89d4f0ce7c66874f4c19682190873dba9de0
kourjot <pkour6188@gmail.com>
2025-05-02 22:20:22 +0530

    array of student

---
+++ javaScriptBasic/q1.js
@@ -0,0 +1,31 @@
+// Given an array of student objects with name and marks (array of numbers), return a new array of student names sorted in descending order of their total marks.
+
+// op=["Diya", "Aarav", "Kabir"]
+
+
+let arr=[
+    { name: "Aarav", marks: [88, 76, 92] },
+    { name: "Diya", marks: [99, 91, 85] },
+    { name: "Kabir", marks: [72, 89, 68] }
+]
+
+function solve(arr){
+    let res=[]
+    for(let i=0;i<arr.length;i++){
+        let std=arr[i]
+        let sum=0
+        for(let j=0;j<std.marks.length;j++){
+            sum+=std.marks[j]
+        }
+        res.push({name:std.name,marks:sum})
+    }
+    let x=res.sort((a,b)=>b.marks -a.marks)
+    let op=[]
+    for(let i=0;i<x.length;i++){
+        op.push(res[i].name)
+        
+    }
+    console.log(op)
+}
+
+solve(arr)
